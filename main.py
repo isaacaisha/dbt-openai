96899bb55d6a7fd8f9fc4c827c453f420761fee4
isaacaisha <medusadbt@gmail.com>
2023-09-03 01:35:21 +0200

    add some style to the app

--- main.py
+++ main.py
@@ -118,4 +118,4 @@ if __name__ == '__main__':
     if os.path.exists(temp_audio_file):
         os.remove(temp_audio_file)
 
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
