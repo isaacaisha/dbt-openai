95836af9fe24d32b4c472efa1de9f83de28a16e4
isaacaisha <medusadbt@gmail.com>
2023-10-15 13:05:34 +0200

    test the openai memory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify, send_file, session
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField, validators
 from openai.error import RateLimitError
@@ -7,38 +7,64 @@ import os
 import re
 import openai
 from gtts import gTTS
+import warnings
+from dotenv import load_dotenv, find_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+import sqlite3
 
+# Get the current date
+current_date = datetime.now().date()
 
-app = Flask(__name__)
-app.secret_key = "any-string-you-want-just-keep-it-secret"
+# Define the date after which the model should be set to "gpt-3.5-turbo"
+target_date = datetime(2024, 6, 12).date()
+
+# Set the model variable based on the current date
+if current_date > target_date:
+    llm_model = "gpt-3.5-turbo"
+else:
+    llm_model = "gpt-3.5-turbo-0301"
+
+# Load environment variables from .env file
+_ = load_dotenv(find_dotenv())
 
 # Set up OpenAI API credentials
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-# ------------------------------------------------------ VARIABLES ----------------------------------------------------#
-# Initialize an empty conversation list
-conversation = []
+# Initialize Flask app and SQLite database connection
+app = Flask(__name__)
+app.secret_key = os.urandom(25)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///conversation_history.db'
+
+# Suppress warnings
+warnings.filterwarnings('ignore')
+
+# Initialize SQLite database
+with app.app_context():
+    db = sqlite3.connect('conversation_history.db')
+    cursor = db.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS conversation (role TEXT, content TEXT)')
+    db.commit()
+    db.close()
 
 
-# -------------------------------------------------------- CLASS ------------------------------------------------------#
 class TextAreaForm(FlaskForm):
     writing_text = TextAreaField('Start Writing', [validators.InputRequired(message="Please enter text.")])
     submit = SubmitField()
 
 
-# ------------------------------------------------------- FUNCTION ----------------------------------------------------#
 @app.route("/", methods=["GET", "POST"])
 def home():
     writing_text_form = TextAreaForm()
-    error_message = None  # Initialize an error message variable
-    answer = None  # Initialize the generated text variable
+    error_message = None
+    answer = None
 
     if request.method == "POST" and writing_text_form.validate_on_submit():
-        # writing_text_data = request.form['writing_text']  # Get the input from the textarea
+        user_message = request.form['writing_text']
 
         try:
-            # Process the answer before passing it to the template
-            answer = post_process_answer(answer)
+            answer = get_openai_response(user_message)
         except RateLimitError:
             error_message = "You exceeded your current quota, please check your plan and billing details."
 
@@ -51,38 +77,61 @@ def answer():
     user_message = request.form['prompt']
     print(f'User Input:\n{user_message} 😎\n')
 
-    # Extend the conversation with the user's message
-    conversation.append({
-        "role": "user",
-        "content": user_message
-    })
+    # Store the conversation in the SQLite database
+    with app.app_context():
+        db = sqlite3.connect('conversation_history.db')
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO conversation (role, content) VALUES (?, ?)', ('user', user_message))
+        db.commit()
+        db.close()
 
-    # Use OpenAI's GPT to generate the answer based on the conversation
-    response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=conversation,
-        temperature=0  # this is the degree of randomness model's output
-    )
 
-    # Get the assistant's reply
-    assistant_reply = response.choices[0].message['content']
+    # Retrieve the entire conversation history from the database
+    with app.app_context():
+        db = sqlite3.connect('conversation_history.db')
+        cursor = db.cursor()
+        cursor.execute('SELECT role, content FROM conversation')
+        conversation_data = cursor.fetchall()
+        db.close()
 
-    # Post-process the answer for readability (if needed)
-    assistant_reply = post_process_answer(assistant_reply)
+        # Debug print to check conversation data
+        print(conversation_data)
 
-    # Convert the text response to speech using gTTS
-    tts = gTTS(assistant_reply)
+    conversation = []
 
-    # Create a temporary audio file
+    for role, content in conversation_data:
+        conversation.append({
+            "role": role,
+            "content": content
+        })
+
+    response = get_openai_response(user_message, conversation)
+    response = post_process_answer(response)
+
+    tts = gTTS(response)
     audio_file_path = 'temp_audio.mp3'
     tts.save(audio_file_path)
-    print(f'Chat GPT Response:\n{assistant_reply} 😝\n')
+    print(f'Chat GPT Response:\n{response} 😝\n')
 
-    # Return the response as JSON, including both text and the path to the audio file
     return jsonify({
-        "answer_text": assistant_reply,
+        "answer_text": response,
         "answer_audio_path": audio_file_path,
     })
+
+
+def get_openai_response(user_message, conversation):
+    response = ChatOpenAI(temperature=0.0, model=llm_model)
+
+    # Use the conversation history passed as an argument
+    memory = ConversationBufferMemory(conversation=conversation)
+    conversation_ = ConversationChain(
+        llm=response,
+        memory=memory,
+        verbose=False
+    )
+
+    response = conversation_.predict(input=user_message)
+    return response
 
 
 # Serve the audio file
@@ -92,24 +141,15 @@ def serve_audio():
     return send_file(audio_file_path, as_attachment=True)
 
 
-# Define a function for post-processing the answer
 def post_process_answer(answer):
-    # You can add your post-processing logic here,
-    # For example, you can filter out unwanted content or format the answer
-    # Example post-processing:
-
-    # Convert newlines to HTML line breaks
     answer = answer.replace('\n', '<br>')
-
-    # Remove HTML tags
     answer = re.sub('<.*?>', '', answer)
     return answer
 
 
 if __name__ == '__main__':
-    # Clean up any previous temporary audio files
     temp_audio_file = 'temp_audio.mp3'
     if os.path.exists(temp_audio_file):
         os.remove(temp_audio_file)
 
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
