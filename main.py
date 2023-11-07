ea3ec4e35d6264cc000485f172e67aa76af7df7b
isaacaisha <medusadbt@gmail.com>
2023-11-07 09:22:21 +0100

    try to create a good fastapi

--- main.py
+++ main.py
@@ -1,118 +1,133 @@
 import os
 import openai
 from dotenv import load_dotenv, find_dotenv
-import secrets
-from flask import Flask, render_template, request, jsonify, send_file
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField, validators
-from datetime import datetime
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from gtts import gTTS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.memory import ConversationSummaryBufferMemory
-import warnings
+from pydantic import BaseModel
+from datetime import datetime
+import logging
+from typing import Optional
 
-warnings.filterwarnings('ignore')
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
 
-app = Flask(__name__)
-_ = load_dotenv(find_dotenv())  # read local .env file
+# Initialize FastAPI
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Load environment variables
+load_dotenv(find_dotenv())
+
+# Set OpenAI API key
 openai.api_key = os.environ['OPENAI_API_KEY']
-# Generate a random secret key
-secret_key = secrets.token_hex(199)
 
-# Set it as the Flask application's secret key
-app.secret_key = secret_key
-
-# Initialize an empty conversation chain
-llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo-0301")  # Set your desired LLM model here
+# Initialize the conversation chain and other components
+llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo-0301")
 memory = ConversationBufferMemory()
 memory_summary = ConversationSummaryBufferMemory(llm=llm, max_token_limit=19)
 conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
 
 
-# Define a Flask form
-class TextAreaForm(FlaskForm):
-    writing_text = TextAreaField('Start Writing', [validators.InputRequired(message="Please enter text.")])
-    submit = SubmitField()
+# Define a Pydantic model for user input
+class UserInput(BaseModel):
+    user_message: str
 
 
-@app.route("/", methods=["GET", "POST"])
-def home():
+class TextAreaForm(BaseModel):
+    userInput: Optional[str] = None
+
+
+# Define a FastAPI route to render the template with the form
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def get_home(request: Request, answer: str = None):
     writing_text_form = TextAreaForm()
+    return templates.TemplateResponse("index.html", {
+        "writing_text_form": writing_text_form,
+        "request": request,
+        "answer": answer,
+        "date": datetime.now().strftime("%a %d %B %Y")
+    })
+
+
+@app.post("/")
+async def home(request: Request, user_input: UserInput = Form(...)):
+    user_input_text = user_input.user_message
     answer = None
 
-    if request.method == "POST" and writing_text_form.validate_on_submit():
-        user_input = request.form['writing_text']
+    user_input = user_input_text
 
-        # Use the LLM to generate a response based on user input
-        response = conversation.predict(input=user_input)
+    response = conversation.predict(input=user_input)
 
-        answer = response['output'] if response else None
+    answer = response['output'] if response else None
 
     memory_load = memory.load_memory_variables({})
     memory_buffer = memory.buffer
 
     memory_summary.save_context({"input": f"Summarize the memory.buffer:"}, {"output": f"{memory_buffer}"})
     summary_buffer = memory_summary.load_memory_variables({})
-    print(f'Summary Buffer:\n{summary_buffer}\n')
 
-    return render_template('index.html', writing_text_form=writing_text_form, answer=answer,
-                           memory_load=memory_load, memory_buffer=memory_buffer, summary_buffer=summary_buffer,
-                           date=datetime.now().strftime("%a %d %B %Y"))
-
-
-@app.route('/answer', methods=['POST'])
-def answer():
-    user_message = request.form['prompt']
-    print(f'User Input:\n{user_message} 😎\n')
-
-    # Extend the conversation with the user's message
-    response = conversation.predict(input=user_message)
-
-    # Check if the response is a string, and if so, use it as the assistant's reply
-    if isinstance(response, str):
-        assistant_reply = response
-    else:
-        # If it's not a string, access the assistant's reply as you previously did
-        assistant_reply = response.choices[0].message['content']
-
-    # Convert the text response to speech using gTTS
-    tts = gTTS(assistant_reply)
-
-    # Create a temporary audio file
-    audio_file_path = 'temp_audio.mp3'
-    tts.save(audio_file_path)
-    print(f'LLM Response:\n{assistant_reply} 😝\n')
-
-    # Return the response as JSON, including both text and the path to the audio file
-    return jsonify({
-        "answer_text": assistant_reply,
-        "answer_audio_path": audio_file_path,
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "answer": answer,
+        "memory_load": memory_load,
+        "memory_buffer": memory_buffer,
+        "summary_buffer": summary_buffer,
+        "date": datetime.now().strftime("%a %d %B %Y")
     })
 
 
-@app.route('/audio')
+@app.post("/answer")
+def answer(user_message: str = Form(...)):
+    response = conversation.predict(input=user_message)
+
+    if isinstance(response, str):
+        assistant_reply = response
+    else:
+        assistant_reply = response.choices[0].message['content']
+
+    tts = gTTS(assistant_reply)
+
+    audio_file_path = 'temp_audio.mp3'
+    tts.save(audio_file_path)
+
+    return {
+        "answer_text": assistant_reply,
+        "answer_audio_path": audio_file_path,
+    }
+
+
+@app.get('/audio')
 def serve_audio():
     audio_file_path = 'temp_audio.mp3'
-    return send_file(audio_file_path, as_attachment=True)
+    return FileResponse(audio_file_path, media_type='audio/mpeg', headers={"Content-Disposition": "attachment"})
 
 
-@app.route('/show-history')
-def show_story():
+@app.get("/show-history", response_class=HTMLResponse)
+async def show_history(request: Request):
     memory_load = memory.load_memory_variables({})
     memory_buffer = memory.buffer
-    print(f'Memory Buffer:\n{memory_buffer}\n')
-    print(f'Memory Load:\n{memory_load}\n')
-    print(f'Conversation:\n{conversation}\n')
 
     memory_summary.save_context({"input": f"Summarize the conversation:"}, {"output": f"{conversation}"})
     summary_conversation = memory_summary.load_memory_variables({})
-    print(f'Summary Conversation:\n{summary_conversation}\n')
 
-    return render_template('show-history.html', memory_load=memory_load, memory_buffer=memory_buffer,
-                           conversation=conversation, summary_conversation=summary_conversation,
-                           date=datetime.now().strftime("%a %d %B %Y"))
+    return templates.TemplateResponse("show-history.html", {
+        "request": request,
+        "memory_load": memory_load,
+        "memory_buffer": memory_buffer,
+        "conversation": conversation,
+        "summary_conversation": summary_conversation,
+        "date": datetime.now().strftime("%a %d %B %Y")
+    })
 
 
 if __name__ == '__main__':
@@ -121,4 +136,6 @@ if __name__ == '__main__':
     if os.path.exists(temp_audio_file):
         os.remove(temp_audio_file)
 
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    import uvicorn
+
+    uvicorn.run(app, host='127.0.0.1', port=int(os.environ.get('PORT', 8000)))
