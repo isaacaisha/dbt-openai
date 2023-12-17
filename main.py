f81374149ef938288cf158457bb307012c79a1e3
Your GitHub Username <Your GitHub Email>
2023-12-17 21:34:01 +0100

    add time.sleep()

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ def load_user(user_id):
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    time.sleep(1)
     form = RegisterForm()
 
     try:
@@ -138,10 +139,12 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    time.sleep(1)
     form = LoginForm()
 
     try:
         if form.validate_on_submit():
+            time.sleep(1)
             email = request.form.get('email')
             password = request.form.get('password')
             remember_me = form.remember_me.data
@@ -202,6 +205,7 @@ def home():
 
 @app.route("/conversation-answer", methods=["GET", "POST"])
 def conversation_answer():
+    time.sleep(1)
     writing_text_form = TextAreaForm()
     answer = None
     owner_id = None
@@ -340,6 +344,7 @@ def serve_audio():
 
 @app.route('/show-history')
 def show_story():
+    time.sleep(1)
 
     try:
         if current_user.is_authenticated:
@@ -367,6 +372,7 @@ def show_story():
 
 @app.route("/get-all-conversations")
 def get_all_conversations():
+    time.sleep(1)
 
     try:
         if current_user.is_authenticated:
@@ -409,12 +415,14 @@ def get_all_conversations():
 
 @app.route('/select-conversation-id', methods=['GET', 'POST'])
 def select_conversation():
+    time.sleep(1)
 
     try:
         if current_user.is_authenticated:
             form = ConversationIdForm()
 
             if form.validate_on_submit():
+                time.sleep(1)
                 # Retrieve the selected conversation ID
                 selected_conversation_id = form.conversation_id.data
 
@@ -436,6 +444,7 @@ def select_conversation():
 
 @app.route('/conversation/<int:conversation_id>')
 def get_conversation(conversation_id):
+    time.sleep(1)
 
     try:
         # Retrieve the conversation by ID
@@ -445,6 +454,7 @@ def get_conversation(conversation_id):
 
             if conversation_ is not None and current_user.is_authenticated:
                 if conversation_.owner_id == current_user.id:
+                    time.sleep(1)
                     # Format created_at timestamp
                     formatted_created_at = conversation_.created_at.strftime("%a %d %B %Y %H:%M:%S")
 
@@ -473,12 +483,14 @@ def get_conversation(conversation_id):
 
 @app.route('/delete-conversation', methods=['GET', 'POST'])
 def delete_conversation():
+    time.sleep(1)
 
     try:
         if current_user.is_authenticated:
             form = DeleteForm()
 
             if form.validate_on_submit():
+                time.sleep(1)
                 # Access the database session using the get_db function
                 with get_db() as db:
                     # Get the conversation_id from the form
