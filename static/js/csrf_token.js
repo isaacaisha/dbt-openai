06030e8b005902128a415133ae1dc4ac6f9ba66d
Your GitHub Username <Your GitHub Email>
2023-12-27 13:36:12 +0100

    try change into crsf_token.js

--- static/js/csrf_token.js
+++ static/js/csrf_token.js
@@ -1,65 +1,44 @@
-// Function to retrieve the CSRF token from the hidden span
-function getCSRFToken() {
-    return document.getElementById('csrf-token').textContent;
-}
+// Function to handle AJAX requests with CSRF token inclusion using fetch
+async function makeAjaxRequest(method, url, data) {
+    try {
+        const response = await fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-CSRFToken': getCSRFToken(),
+            },
+            body: new URLSearchParams(data),
+        });
 
-// Function to handle AJAX requests with CSRF token inclusion
-function makeAjaxRequest(method, url, data, successCallback, errorCallback) {
-    // Create a new XMLHttpRequest object
-    const xhr = new XMLHttpRequest();
-
-    // Define the type of request, the URL, and whether the request should be asynchronous
-    xhr.open(method, url, true);
-
-    // Set the request header to include the CSRF token
-    xhr.setRequestHeader('X-CSRFToken', getCSRFToken());
-
-    // Define the callback functions to handle the response
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            successCallback(xhr.responseText);
-        } else {
-            errorCallback(xhr.status, xhr.statusText);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
-    };
 
-    xhr.onerror = function () {
-        errorCallback(xhr.status, xhr.statusText);
-    };
-
-    // Convert the data object to a URL-encoded string
-    const formData = new URLSearchParams();
-    for (const key in data) {
-        formData.append(key, data[key]);
+        const responseData = await response.text();
+        return responseData;
+    } catch (error) {
+        console.error('Error:', error.message);
     }
-
-    // Send the request with the form data
-    xhr.send(formData);
 }
 
-// Function to handle form submission with AJAX
+// Function to handle form submission with AJAX using fetch
 function handleFormSubmission(event) {
-    // Prevent the default form submission behavior
     event.preventDefault();
 
-    // Get the form associated with the submit button
     const form = event.target.closest('form');
 
     if (form) {
-        // Extract form data
         const formData = new FormData(form);
 
-        // Call the generic AJAX request function
-        makeAjaxRequest('POST', form.action, formData,
-            function (response) {
+        makeAjaxRequest('POST', form.action, formData)
+            .then((response) => {
                 // Handle success response here
                 console.log(response);
-            },
-            function (status, statusText) {
+            })
+            .catch((error) => {
                 // Handle error here
-                console.error('Error:', status, statusText);
-            }
-        );
+                console.error('Error:', error);
+            });
     }
 }
 
