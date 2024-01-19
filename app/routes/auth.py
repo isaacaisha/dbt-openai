033a52a25068f3f1233aeff905a5f2eeb8aedf88
Your GitHub Username <Your GitHub Email>
2024-01-19 12:43:56 +0100

    test

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
@@ -12,35 +12,29 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
     register_form = RegisterForm()
-
     if register_form.validate_on_submit():
-        print(f"Form data: {register_form.data}")
 
-        # # Check if the passwords match
-        # if register_form.password.data != register_form.confirm_password.data:
-        #     flash("Passwords do not match. Please enter matching passwords ¡!¡😭¡!¡")
-        #     return redirect(url_for('register'))
-#
         # If user's email already exists
-        if User.query.filter_by(email=register_form.email.data.lower().strip()).first():
+        if User.query.filter_by(email=register_form.email.data).first():
+            print(User.query.filter_by(email=register_form.email.data).first())
             # Send a flash message
-            flash("You've already signed up with that email, log in instead! ¡!!🤣¡!¡")
+            flash("You've already signed up with that email, log in instead! 🤣")
             return redirect(url_for('login'))
 
         hash_and_salted_password = generate_password_hash(
-            register_form.password.data,
+            request.form.get('password'),
             method='pbkdf2:sha256',
             salt_length=8
         )
 
         new_user = User()
-        new_user.email = register_form.email.data.lower().strip()
-        new_user.name = register_form.name.data
+        new_user.email = request.form['email']
+        new_user.name = request.form['name']
         new_user.password = hash_and_salted_password
+        # new_user.is_admin = True # Set this user as an admin
 
         db.session.add(new_user)
         db.session.commit()
-        db.session.refresh(new_user)
 
         # Log in and authenticate the user after adding details to the database.
         login_user(new_user)
@@ -56,22 +50,20 @@ def login():
     login_form = LoginForm()
 
     if login_form.validate_on_submit():
-        print(f"Form data: {login_form.data}")
-
-        email = login_form.email.data.lower().strip()
-        password = login_form.password.data.lower().strip()
+        email = request.form.get('email')
+        password = request.form.get('password')
         remember_me = login_form.remember_me.data
 
+        # Find user by email entered.
         user = User.query.filter_by(email=email).first()
 
         # Email doesn't exist
         if not user:
-            flash("That email does not exist, try again ¡!¡😭¡!¡")
+            flash("That email does not exist, please try again 😭")
             return redirect(url_for('login'))
-
         # Password incorrect
         elif not check_password_hash(user.password, password):
-            flash('Password incorrect, please try again ¡!¡😭¡!¡')
+            flash('Password incorrect, please try again 😭')
             return redirect(url_for('login'))
 
         # Email exists and password correct
