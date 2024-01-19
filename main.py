dbd55ba4093368adf3c34c5fe8ae8ea85f76b374
Your GitHub Username <Your GitHub Email>
2024-01-19 14:17:59 +0100

    remove "@logged_rquered" decorator

--- main.py
+++ main.py
@@ -59,7 +59,6 @@ def home_audio():
     return audio_home()
 
 
-@login_required
 @app.route('/conversation-interface', methods=['GET', 'POST'])
 def conversation_interface():
     return process_interface_llm()
@@ -88,37 +87,31 @@ def interface_serve_audio():
     return process_interface_audio_llm()
 
 
-@login_required
 @app.route("/show-history")
 def show_story():
     return history()
 
 
-@login_required
 @app.route("/get-all-conversations")
 def get_all_conversations():
     return all_conversation_llm()
 
 
-@login_required
 @app.route('/select-conversation-id', methods=['GET', 'POST'])
 def select_conversation():
     return conversation_selected()
 
 
-@login_required
 @app.route('/conversation/<int:conversation_id>')
 def get_conversation(conversation_id):
     return access_conversation(conversation_id)
 
 
-@login_required
 @app.route('/delete-conversation', methods=['GET', 'POST'])
 def delete_conversation():
     return conversation_deleted()
 
 
-@login_required
 @app.route("/api/conversations-jsonify")
 def get_conversations_jsonify():
     return conversation_jsonify_llm()
