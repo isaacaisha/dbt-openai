0f11d7a3b7e69170040886ca0c4e593af7b9d177
Your GitHub Username <Your GitHub Email>
2024-02-20 09:34:16 +0100

    try a diferent approch to ensure the user active state after login in

--- app/static/js/active-state.js
+++ app/static/js/active-state.js
@@ -1,12 +1,19 @@
-// Get all navigation links except for the class -> remove-active-state 
-var navLinks = document.querySelectorAll('.nav-link:not(.remove-active-state)');
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.nav-link');
+    var currentUrl = '{{ current_url }}';  // Use Flask to pass the current URL from the server
 
-// Get the current URL
-var currentUrl = window.location.pathname;
+    navLinks.forEach(function (link) {
+        if (link.getAttribute('href') === currentUrl) {
+            link.classList.add('active');
+        }
+    });
 
-// Loop through each navigation link and add the active class if the URL matches
-navLinks.forEach(function (link) {
-    if (link.getAttribute('href') === currentUrl) {
-        link.classList.add('active');
-    }
+    // Update active state when user navigates using history API
+    window.addEventListener('popstate', function () {
+        var currentUrl = window.location.pathname;
+
+        navLinks.forEach(function (link) {
+            link.classList.toggle('active', link.getAttribute('href') === currentUrl);
+        });
+    });
 });
