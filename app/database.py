6b309ef8ce7cf3c1e1c999426cc0e972be0b8a38
Your GitHub Username <Your GitHub Email>
2024-06-01 00:28:29 +0200

    try to fix deploy bugs

--- app/database.py
+++ app/database.py
@@ -15,6 +15,7 @@ host = os.getenv('DB_HOST')
 port = os.getenv('DB_PORT')
 database = os.getenv('DB_NAME')
 
+
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
 )
@@ -23,6 +24,6 @@ SQLALCHEMY_DATABASE_URL = (
 db = SQLAlchemy()
 
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
