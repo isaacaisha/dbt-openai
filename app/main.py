76d1b099787d39d257dc597e10d1692af22ef83b
Your GitHub Username <Your GitHub Email>
2024-06-01 00:40:55 +0200

    try  o fix bugs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import secrets
 from datetime import timedelta
 
@@ -8,7 +9,9 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
-# Updated imports to reflect the correct structure
+# Ensure current directory is in the sys.path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from database import SQLALCHEMY_DATABASE_URL, db, database_bp
 from app_forms import app_form_bp
 from memory import memory_bp, User
@@ -19,7 +22,6 @@ from routes.home_process import home_conversation_bp
 from routes.convers_functions import conversation_functionality_bp
 from routes.llm_conversation import llm_conversation_bp
 
-# Load environment variables
 load_dotenv(find_dotenv())
 
 def create_app():
@@ -43,15 +45,11 @@ def create_app():
     except KeyError:
         raise ValueError("OPENAI_API_KEY environment variable is not set.")
 
-    # Set the OpenAI API key
     openai.api_key = openai_api_key
 
-    # Generate a random secret key
     secret_key = secrets.token_hex(19)
-    # Set it as the Flask application's secret key
     app.secret_key = secret_key
 
-    # Set SQLAlchemy configurations
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JSON_AS_ASCII'] = False
@@ -61,7 +59,6 @@ def create_app():
     with app.app_context():
         db.create_all()
 
-    # Register blueprints
     app.register_blueprint(database_bp, name='database')
     app.register_blueprint(app_form_bp, name='forms')
     app.register_blueprint(memory_bp, name='memory')
@@ -76,7 +73,6 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    # Clean up any previous temporary audio files
     home_temp_audio_file = 'home_temp_audio.mp3'
     if os.path.exists(home_temp_audio_file):
         os.remove(home_temp_audio_file)
@@ -86,4 +82,3 @@ if __name__ == '__main__':
         os.remove(interface_temp_audio_file)
 
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-    
